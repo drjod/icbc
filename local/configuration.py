34b1144d7d86561560ca1c193ea71d439a2b56d0
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2015-10-01 20:28:18 +0200

    add operations: import, export, clean

---
+++ local/configuration.py
@@ -0,0 +1,11 @@
+
+
+
+inputFileEndings = ['bc', 'fct', 'gli', 'ic', 'mcp', 'mfp', 'mmp', 'msh', 'msp', 'num', 'out', 'pcs', 'rfd', 'st', 'tim']
+outputFileEndings = ['asc', 'tec', 'txt']
+
+ 
+
+
+
+

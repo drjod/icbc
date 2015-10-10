66e1a093f3fae000057d1ab9895f38f4daa77d5a
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2015-10-10 19:20:35 +0200

    Update run.py

--- local/run.py
+++ local/run.py
@@ -2,7 +2,7 @@
 import environment
 
 
-environment = environment.Environment(computer='amak', code='ogs', user = 'delfs', mySQL_password='so3bt4jc'  )
+environment = environment.Environment(computer='amak', code='ogs', user = 'delfs', mySQL_password='*****'  )
 environment.loop()
 
  

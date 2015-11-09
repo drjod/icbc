52c3552f86ade03aa57c8f0c745a9c917a1bef62
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2015-11-09 18:34:22 +0100

    initial commit

---
+++ synchronization/keepUptoDate_sourceCode_ogs_kb1_rzcluster.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set localComputer=amak
+set remoteComputer=rzcluster
+set code=ogs
+set branch=ogs_kb1
+set login=sungw389
+set hostname=rzcluster.rz.uni-kiel.de
+
+call F:\testingEnvironment\scripts\icbc\pwds\%remoteComputer%.bat
+
+call F:\testingEnvironment\scripts\icbc\synchronization\keepUpToDate_sourceCode.bat 

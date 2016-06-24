ca29d65818290bea00731e0c71580dd582dd33e3
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2016-06-24 23:01:10 +0200

    update

--- shared/environment.py
+++ shared/environment.py
@@ -18,7 +18,6 @@ class Environment:
   
     __reselectFlag = False
     __currentOperation = None # used for reselect (operation)
-    __level = None
     ##############################################
     #  Environment: constructor
     #  Task:
@@ -28,20 +27,21 @@ class Environment:
     
     def __init__( self, superuser = None, computer = None, user = None, code = None, branch = None, 
                   typeList = [None], caseList = [[None]], configurationList = [None], 
-                  operationType = '',     # [b,t]
+                  operationType = None,     # [b,t,p]
                   operation = None, 
-                  testingDepth = None,	
-                  level = None,						  
+                  testMode = False,
+                  state = None,						  
                   mySQL_user='root', mySQL_password='*****', mySQL_host='localhost', mySQL_schema='testing_environment' ):
                   
         mySQL_struct = setting.mySQL ( mySQL_user, mySQL_password, mySQL_host, mySQL_schema )
         self.__subject_inst = subject.Subject( superuser, computer, user, code, branch  )                                                              
         self.__setting_inst = setting.Setting ( typeList, caseList, configurationList, 
                                                 operationType, operation, 
-                                                testingDepth, 
+                                                testMode, 
                                                 mySQL_struct )
         #self.__simulationData = simulationData.SimulationData()
-        self.__level = level
+        self.__testMode = testMode
+        self.__state = state
 
         if int( configurationCustomized.verbosity ) > 1:
             # print message for already set variables
@@ -67,8 +67,8 @@ class Environment:
                 utilities.message( type='INFO', text='Set operation type ' + operationType )
             if not operation:
                 utilities.message( type='INFO', text='Set operation ' + operation )           
-            if not testingDepth:
-                utilities.message( type='INFO', text='Set testing depth ' + testingDepth )                            
+            if not state:
+                utilities.message( type='INFO', text='Set state ' + state )                            
             if not simulationData:
                 __simulationData = simulationData
                 utilities.message( type='INFO', text='Simulation data set' )  
@@ -88,6 +88,7 @@ class Environment:
                             
     def run( self ): 
         #selects
+        
         if configurationCustomized.location == 'local':
             self.__setting_inst.connectToMySQL()   
         self.__subject_inst.select( self.__setting_inst )  
@@ -115,21 +116,21 @@ class Environment:
                 if selectedOperationType == 'b': # building (only configuration)
                     self.__setting_inst.setTypeList( [None] ) 
                     self.__setting_inst.setCaseList( [[None]] )                          
-                    self.__setting_inst.setConfigurationList( self.__setting_inst.selectItemConstituentGroup( groupType = 'configurations',
+                    self.__setting_inst.setConfigurationList( self.__setting_inst.setItemConstituents( groupType = 'configurations',
                                                                                                               computerOfSubject = self.__subject_inst.getComputer() ) )                        
                 elif selectedOperationType == 's':  # simulating (all item constituents (type, case, configuration))
-                    self.__setting_inst.setTypeList( self.__setting_inst.selectItemConstituentGroup( groupType = 'types' ) ) 
-                    self.__setting_inst.setCaseList( self.__setting_inst.selectItemConstituentGroup( groupType = 'cases' ) )                                      
-                    self.__setting_inst.setConfigurationList( self.__setting_inst.selectItemConstituentGroup( groupType = 'configurations',
+                    self.__setting_inst.setTypeList( self.__setting_inst.setItemConstituents( groupType = 'types' ) ) 
+                    self.__setting_inst.setCaseList( self.__setting_inst.setItemConstituents( groupType = 'cases' ) )                                      
+                    self.__setting_inst.setConfigurationList( self.__setting_inst.setItemConstituents( groupType = 'configurations',
                                                                                                               computerOfSubject = self.__subject_inst.getComputer() ) ) 
                 elif selectedOperationType == 'p':  # plotting
-                    self.__setting_inst.setTypeList( self.__setting_inst.selectItemConstituentGroup( groupType = 'types' ) )
-                    if self.__currentOperation == 'j': # generateJPGs (only type)
+                    self.__setting_inst.setTypeList( self.__setting_inst.setItemConstituents( groupType = 'types' ) )
+                    if self.__currentOperation == 'j' or self.__currentOperation == 'w': # generate or wait for JPG (only type)
                         self.__setting_inst.setCaseList( [[None]] ) 
                         self.__setting_inst.setConfigurationList( [None] ) 
                     else:   # all item constituents  (type, case, configuration)
-                        self.__setting_inst.setCaseList( self.__setting_inst.selectItemConstituentGroup( groupType = 'cases' ) )                                      
-                        self.__setting_inst.setConfigurationList( self.__setting_inst.selectItemConstituentGroup( groupType = 'configurations',
+                        self.__setting_inst.setCaseList( self.__setting_inst.setItemConstituents( groupType = 'cases' ) )                                      
+                        self.__setting_inst.setConfigurationList( self.__setting_inst.setItemConstituents( groupType = 'configurations',
                                                                                                                   computerOfSubject = self.__subject_inst.getComputer() ) )  
             # loop over items 
             if self.__reselectFlag == True:
@@ -142,12 +143,12 @@ class Environment:
                 for type in self.__setting_inst.getItemConstituents().typeList:                   
                     for case in self.__setting_inst.getItemConstituents().caseList[typeList_counter]:
                         for configuration in self.__setting_inst.getItemConstituents().configurationList:
-        
-                            if not self.__level and case:  # (string empty) should not be the case if on remote computer
-                                self.__level = self.__setting_inst.getLevel( case ) # database query
+                            
+                            if not self.__state and case:  # (string empty) value must have been set if on remote computer
+                                self.__state = self.__setting_inst.getState( case )    # database query  
                             else:
-                                self.__level = '1'
-
+                                self.__state = '1'   # is active
+                            
                             if selectedOperationType == 'b': # building 
                                 item_inst = item.Build( self.__subject_inst, configuration )
                             elif selectedOperationType == 's': # simulating
@@ -159,18 +160,23 @@ class Environment:
 
                             simData = simulationData.SimulationData( selectedOperationType, selectedOperation )
                             if configurationCustomized.location == 'local': 
-                                # do mysql queries 
-                                if simData.getReadFileFlags()._numerics  == True: 
-                                    simData = self.__setting_inst.setNumData( simData, type, case, configuration )                         
-                                    simData.writeNumData()
-                                if simData.getReadFileFlags()._processing  == True: 
-                                    simData = self.__setting_inst.setProcessingData( simData, type, case, configuration )
-                                    simData.writeProcessingData()
-
-                            operation_inst.run( item_inst, simData, self.__level ) 
-                            self.__level = None # since level depends on item
-
+                                if not self.__testMode or self.__state == '1':
+                                    #      do mysql queries 
+                                    if simData.getReadFileFlags()._numerics  == True: 
+                                        simData = self.__setting_inst.setNumData( simData, type, case, configuration )  
+                                        if self.__subject_inst.getLocation() == 'remote':  
+                                            # file transfer              
+                                            simData.writeNumData( configuration )
+                                    if simData.getReadFileFlags()._processing  == True: 
+                                        simData = self.__setting_inst.setProcessingData( simData, type, case, configuration )
+                                        if self.__subject_inst.getLocation() == 'remote':  
+                                             # file transfer 
+                                            simData.writeProcessingData( configuration )
+                            operation_inst.run( item_inst, simData, self.__state, self.__testMode ) 
+                            self.__state = None # since state depends on item
+                             
                             del simData
+                            
 
                     del item_inst   
                     if len( self.__setting_inst.getItemConstituents().caseList ) > 1:       
@@ -182,7 +188,7 @@ class Environment:
         
         if configurationCustomized.location == 'local':
             self.__setting_inst.disconnectFromMySQL()  # to reconnect each run in case of table update
-          
+        
         if not self.__setting_inst.getPreselectedOperation():   
             self.run()   
              

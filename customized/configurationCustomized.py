dba2ef3797d6764df6627b0e593363e62a24cbd1
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2016-06-24 22:59:42 +0200

    update

--- customized/configurationCustomized.py
+++ customized/configurationCustomized.py
@@ -1,7 +1,7 @@
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'shared'))
 
-     
+outputFile = 'out.txt'
 location = 'local'
 rootDirectory = 'F:\\'
 computer = 'amak'
@@ -14,7 +14,8 @@ preplot = "\"C:\\Program Files\\Tecplot\\Tec360 2013R1\\bin\\preplot.exe\""
 tecplot = "\"C:\\Program Files\\Tecplot\\Tec360 2013R1\\bin\\tec360.exe\"" 
 winscp="\"C:\\Program Files (x86)\\WinSCP\\WinSCP.com\""
 # visualStudio = "\"C:\\Program Files (x86)\\Microsoft Visual Studio 12.0\\Common7\\IDE\devenv.exe"" set in batch file  
-localBuild = rootDirectory + "testingEnvironment\\scripts\\icbc\\batches\\buildAll\\buildAsAGAdmin.bat"
+localBuild = rootDirectory + "testingEnvironment\\scripts\\icbc\\gateToWindows\\build\\buildAsAGAdmin.bat"
+localRun = rootDirectory + "testingEnvironment\\scripts\\icbc\\gateToWindows\\run\\RunAsAGAdmin.bat"
 
 
 

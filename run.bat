bfe406146f592687c41b300f2db185875a19a463
Jens-Olaf Delfs <jod@gpi.uni-kiel.de>
2016-12-05 00:37:09 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
-C:\\Users\\"Jens-Olaf Delfs"\\Anaconda3\\python.exe shared\\icbc.py
-# C:\\Python34\\python.exe shared\\icbc.py
+C:\\Users\\"Jens-Olaf Delfs"\\Anaconda3\\python.exe icbc.py
+# C:\\Python34\\python.exe icbc.py
 
 
 
